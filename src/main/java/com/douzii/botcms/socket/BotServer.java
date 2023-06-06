7835a60232a5d57e35dc9d6f917b99f3508dd212
3527231 <2507274689@qq.com>
2023-06-06 16:21:05 +0800

    日志请求

--- src/main/java/com/douzii/botcms/socket/BotServer.java
+++ src/main/java/com/douzii/botcms/socket/BotServer.java
@@ -23,7 +23,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -69,11 +71,34 @@ public class BotServer {
             log.info(qq+"该账号已登录");
             return;
         }
+        File log = new File("log/" + qq + "/" + new SimpleDateFormat("YYYY-MM-dd").format(new Date()) + ".log");
+        if (log.exists()){
+            log.delete();
+        }
 
         this.bot = BotFactory.INSTANCE.newBot(qq, BotAuthorization.byQRCode(), configuration -> {
             configuration.setProtocol(BotConfiguration.MiraiProtocol.ANDROID_WATCH);
             configuration.setLoginSolver(botLoginSolver);
-            configuration.setBotLoggerSupplier(bot1 -> new BotLogger("Bot", (logPriority, s, throwable) -> {
+
+
+
+
+            configuration.setBotLoggerSupplier(bot1 -> new SimpleLogger("Bot", (logPriority, s, throwable) -> {
+
+                String path = "log/" + bot1.getId();
+                File file = new File(path);
+                if (!file.exists()){
+                    file.mkdirs();
+                }
+                File file1 = new File(path + "/" + new SimpleDateFormat("YYYY-MM-dd").format(new Date()) + ".log");
+
+                if (!file1.exists()){
+                    try {
+                        file1.createNewFile();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
 
                 List<LogServer> list = LogServer.webSockets.stream().filter(logServer -> logServer.getQq() == bot1.getId()).toList();
                 if (list.size() == 0){
@@ -83,13 +108,23 @@ public class BotServer {
 
                 StringBuilder builder = new StringBuilder();
                 builder
-                        .append(new SimpleDateFormat("YYYY-MM-dd").format(new Date()))
-                        .append(" " + logPriority.getNameAligned() + "/" + bot1.getId())
+                        .append("<span class='logTime'>"+new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(new Date())+"</span>")
+                        .append(" <span class='el-tag el-tag--success el-tag--dark'><span class='el-tag__content'>" + logPriority.getNameAligned() + "</span></span><span class='el-avatar el-avatar--circle'><img src='" + bot1.getAvatarUrl() + "' style=\"object-fit: cover;\" /></span>")
                         .append(" " + s);
-                logSession.getAsyncRemote().sendText(builder.toString());
+
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(file1,true));
+                    writer.write(builder.toString());
+                    writer.newLine();
+                    writer.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                logSession.getAsyncRemote().sendText("更新日志");
 
                 return null;
             }));
+
 
         });
         this.thread = new Thread(bot::login);
